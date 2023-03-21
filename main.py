e8a3fd634f2c569907e7a75009448c8384250be0
James Peres <james.peres.s@gmail.com>
2023-03-21 00:34:55 -0300

    add code

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+
+r = requests.get('https://api.telegram.org/bot299717453:AAFJl8cG_xvmMRdRSgs2mlbTPDUCT51V91g/getUpdates')
+print(r.json().get('result')[-1].get('message').get('text'))
+text = r.json().get('result')[-1].get('message').get('text')
+
+
+AUTH_TOKEN = ''
+import gistyc
+
+
+
+file_name_txt = 'file.txt'
+# text = 'teste555!\n'
+
+with open(file_name_txt, 'a') as f:
+    f.write(text)
+
+
+gist_api = gistyc.GISTyc(auth_token=AUTH_TOKEN)
+# response_data = gist_api.create_gist(file_name=file_name_txt)
+
+
+gist_list = gist_api.get_gists()
+for gist in gist_list:
+    file_name_gist = list(gist.get('files').keys())[0]
+    if file_name_gist == file_name_txt:
+        response_update_data = gist_api.update_gist(file_name=file_name_txt, gist_id=gist.get('id'))
+        print(gist.get('id'))
+        print('ok')
+
+
