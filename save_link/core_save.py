980b83eb8c69e734e514b7947bc07c9c9b00a531
James Peres <james.peres.s@gmail.com>
2023-03-26 18:52:08 -0300

    add chalice

---
+++ save_link/core_save.py
@@ -0,0 +1,29 @@
+import requests
+from settings import URL_TELEGRAM, AUTH_TOKEN
+import gistyc
+
+def save_link():
+  r = requests.get(URL_TELEGRAM)
+  text = r.json().get("result")[-1].get("message").get("text")
+  print (text)
+
+  file_name_txt = "file.txt"
+
+  with open(file_name_txt, "a") as f:
+      f.write('\n')
+      f.write(text)
+
+
+  gist_api = gistyc.GISTyc(auth_token=AUTH_TOKEN)
+  # response_data = gist_api.create_gist(file_name=file_name_txt)
+
+  gist_list = gist_api.get_gists()
+  for gist in gist_list:
+      file_name_gist = list(gist.get("files").keys())[0]
+      if file_name_gist == file_name_txt:
+          response_update_data = gist_api.update_gist(
+              file_name=file_name_txt, gist_id=gist.get("id")
+          )
+          print(gist.get("id"))
+          print("ok")
+
